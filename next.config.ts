6bda1b7a94e247cb0e192cd5635df498802c8098
David Vojvodić <davidvojvodic1@gmail.com>
2025-06-28 18:09:58 +0200

    Remove Sentry integration from instrumentation-client and instrumentation files, update global error component to simplify error handling, and delete example Sentry page for cleaner project structure.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,5 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
-import { withSentryConfig } from '@sentry/nextjs';
  
 const withNextIntl = createNextIntlPlugin();
 
@@ -53,35 +52,5 @@ const nextConfig: NextConfig = {
   }
 };
 
-// Export the config wrapped with both plugins
-export default withSentryConfig(
-  withNextIntl(nextConfig),
-  {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
-
-    org: "flowko",
-    project: "flowko-website",
-
-    // Only print logs for uploading source maps in CI
-    silent: !process.env.CI,
-
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
-
-    // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-    tunnelRoute: "/monitoring",
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-
-    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/cron-monitoring/
-    automaticVercelMonitors: true,
-  }
-);
+// Export the config wrapped with next-intl plugin
+export default withNextIntl(nextConfig);
